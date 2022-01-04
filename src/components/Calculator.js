3d91c09b88e506b2224dadcdc8ad47e471d7f2ce
Anjacodes <anja_schmidt7@posteo.de>
2022-01-04 09:16:09 -0600

    Add Styling to Calculator

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,20 +3,28 @@ import React, { Component } from 'react';
 export class Calculator extends Component {
   render() {
     return (
-      <div>
-        <input />
+      <div className="calcContainer">
+        <input className="input"/>
         <div className="gridContainer">
-            <button>AC</button>
-            <button>+/-</button>
-            <button>%</button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
-            <button></button>
+          <button className="btn" type="button">AC</button>
+          <button className="btn" type="button">+/-</button>
+          <button className="btn" type="button">%</button>
+          <button className="btn btn-orange" type="button">÷</button>
+          <button className="btn" type="button">7</button>
+          <button className="btn" type="button">8</button>
+          <button className="btn" type="button">9</button>
+          <button className="btn btn-orange" type="button">x</button>
+          <button className="btn" type="button">4</button>
+          <button className="btn" type="button">5</button>
+          <button className="btn" type="button">6</button>
+          <button className="btn btn-orange" type="button">-</button>
+          <button className="btn" type="button">1</button>
+          <button className="btn" type="button">2</button>
+          <button className="btn" type="button">3</button>
+          <button className="btn btn-orange" type="button">+</button>
+          <button className="btn btn-large" type="button">0</button>
+          <button className="btn" type="button">.</button>
+          <button className="btn btn-orange" type="button">=</button>
         </div>
       </div>
     );
